3f4c0d8ea503ab513eca040eb988dc52a2f34bbe
huzhe <huzhe@hualala.com>
2018-08-25 11:30:09 +0800

    redisHash

--- service/src/main/java/com/hualala/redis/util/RedisHashHandler.java
+++ service/src/main/java/com/hualala/redis/util/RedisHashHandler.java
@@ -5,6 +5,10 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 @Component
 public class RedisHashHandler {
     @Autowired
@@ -18,5 +22,112 @@ public class RedisHashHandler {
      */
     public void put(String key,String hk,String hv){
         redisTemplate.opsForHash().put(key,hk,hv);
+    }
+
+    /**
+     * 对应redis命令：hdel
+     * hdel redisHash name age
+     */
+    public Long delete(String key,String[] arr){
+        return redisTemplate.opsForHash().delete(key,arr);
+    }
+
+    /**
+     * 通过给定的delta增加散列hashKey的值（整型）
+     * 对应redis命令：hincrby
+     * hincrby redisHash grade 2
+     */
+    public Long increment(String key,String hk,long delta){
+        return redisTemplate.opsForHash().increment(key,hk,delta);
+    }
+
+    /**
+     * 通过给定的delta增加散列hashKey的值（浮点数）
+     * 对应redis命令：hincrbyfloat
+     * hincrbyfloat redisHash2 grade 1.0
+     */
+    public Double increment(String key,String hk,double delta){
+        return redisTemplate.opsForHash().increment(key,hk,delta);
+    }
+
+    /**
+     * 获取整个哈希存储根据密钥
+     * 对应redis命令：hgetall
+     * hgetall redisHash2
+     */
+    public Map<Object, Object> entries(String key){
+        return redisTemplate.opsForHash().entries(key);
+    }
+
+    /**
+     * 从键中的哈希获取给定hashKey的值
+     * 对应redis命令：hget
+     * hget redisHash2 name
+     */
+    public String get(String key,String hk){
+        return String.valueOf(redisTemplate.opsForHash().get(key,hk));
+    }
+
+    /**
+     * 确定哈希hashKey是否存在
+     * 对应redis命令：hexists
+     * hexists redisHash2 name
+     */
+    public Boolean hasKey(String key,String hk){
+        return redisTemplate.opsForHash().hasKey(key,hk);
+    }
+
+    /**
+     * 仅当hashKey不存在时才设置散列hashKey的值
+     * 对应redis命令：hsetnx
+     * hsetnx redisHash2 name jack
+     */
+    public Boolean putIfAbsent(String key,String hk,String hv){
+        return redisTemplate.opsForHash().putIfAbsent(key,hk,hv);
+    }
+
+    /**
+     * 获取整个哈希存储的值根据密钥
+     * 对应redis命令：hvals
+     * hvals redisHash2
+     */
+    public List<Object> values(String key){
+        return redisTemplate.opsForHash().values(key);
+    }
+
+    /**
+     * 获取key所对应的散列表的大小个数
+     * 对应redis命令：hlen
+     * hlen redisHash2
+     */
+    public Long size(String key){
+        return redisTemplate.opsForHash().size(key);
+    }
+
+    /**
+     * 获取key所对应的散列表的key
+     * 对应redis命令：hkeys
+     * hkeys redisHash2
+     */
+    public Set<Object> keys(String key){
+        return redisTemplate.opsForHash().keys(key);
+    }
+
+    /**
+     * 使多个散列字段设置到key对应的散列表中
+     * 对应redis命令：hmset
+     * hmset redisHash3 name lili age 12 grade 94
+     */
+    public void putAll(String key,Map<String,String> map){
+        redisTemplate.opsForHash().putAll(key,map);
+    }
+
+    /**
+     * 使多个散列字段设置到key对应的散列表中
+     * 对应redis命令：hmget
+     * hmget redisHash3 name age grade
+     */
+    public List<Object> multiGet(String key,List<Object> hashKeys){
+        return redisTemplate.opsForHash().multiGet(key,hashKeys);
     }
 }
--- service/src/test/java/com/hualala/redis/RedisHashHandlerDemo.java
+++ service/src/test/java/com/hualala/redis/RedisHashHandlerDemo.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.*;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class RedisHashHandlerDemo {
@@ -15,8 +17,88 @@ public class RedisHashHandlerDemo {
 
     @Test
     public void put(){
-        redisHashHandler.put("redisHash","name","jack");
-        redisHashHandler.put("redisHash","age","10");
-        redisHashHandler.put("redisHash","grade","100");
+        redisHashHandler.put("redisHash2","name","tom");
+        redisHashHandler.put("redisHash2","age","12");
+        redisHashHandler.put("redisHash2","grade","90.5");
+    }
+
+    @Test
+    public void delete(){
+        String[] arr = {"name","age","grade"};
+        Long count = redisHashHandler.delete("redisHash", arr);
+        System.out.println(count);//3
+    }
+
+    @Test
+    public void increment1(){
+        Long count = redisHashHandler.increment("redisHash", "grade", 1);
+        System.out.println(count);//91
+    }
+
+    @Test
+    public void increment2(){
+        Double count = redisHashHandler.increment("redisHash2", "grade", 1.0);
+        System.out.println(count);//92.5
+    }
+
+    @Test
+    public void entries(){
+        Map<Object, Object> map = redisHashHandler.entries("redisHash2");
+        System.out.println(map);//{age=12, grade=92.5, name=tom}
+    }
+
+    @Test
+    public void get(){
+        String s = redisHashHandler.get("redisHash2", "name");
+        System.out.println(s);//tom
+    }
+
+    @Test
+    public void hasKey(){
+        Boolean aBoolean = redisHashHandler.hasKey("redisHash2", "name");
+        System.out.println(aBoolean);//true
+    }
+
+    @Test
+    public void putIfAbsent(){
+        Boolean aBoolean = redisHashHandler.putIfAbsent("redisHash2", "name2","jack2");
+        System.out.println(aBoolean);//true
+    }
+
+    @Test
+    public void values(){
+        List<Object> values = redisHashHandler.values("redisHash2");
+        System.out.println(values);//true
+    }
+
+    @Test
+    public void size(){
+        Long size = redisHashHandler.size("redisHash2");
+        System.out.println(size);//5
+    }
+
+    @Test
+    public void keys(){
+        Set<Object> keys = redisHashHandler.keys("redisHash2");
+        System.out.println(keys);//[name, age, grade, name1, name2]
+    }
+
+    @Test
+    public void putAll(){
+        Map<String,String> map = new HashMap<>();
+        map.put("name","lili");
+        map.put("age","12");
+        map.put("grade","94");
+        redisHashHandler.putAll("redisHash3",map);
+    }
+
+    @Test
+    public void multiGet(){
+        List<Object> hashKeys = new ArrayList<>();
+        hashKeys.add("name");
+        hashKeys.add("age");
+        hashKeys.add("grade");
+        List<Object> hash3 = redisHashHandler.multiGet("redisHash3", hashKeys);
+        System.out.println(hash3);//[lili, 12, 94]
     }
 }
